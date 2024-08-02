1bacd7228702df670af1d030f5063eb76d736f3f
ddipass <ddipass@gmail.com>
2024-08-02 17:22:34 +0800

    Correct Frontend for New User Add

--- frontend/src/components/AuthAmplify.tsx
+++ frontend/src/components/AuthAmplify.tsx
@@ -1,11 +1,131 @@
-import React, { ReactNode, cloneElement, ReactElement, useState } from 'react';
+// import React, { ReactNode, cloneElement, ReactElement } from 'react';
+// import { BaseProps } from '../@types/common';
+// import { useTranslation } from 'react-i18next';
+// import { SocialProvider } from '../@types/auth';
+// import { 
+//   Authenticator, 
+//   useAuthenticator, 
+//   useTheme,
+//   View,
+//   Text,
+//   Button
+// } from '@aws-amplify/ui-react';
+
+
+// const MISTRAL_ENABLED: boolean =
+//   import.meta.env.VITE_APP_ENABLE_MISTRAL === 'true';
+
+
+// type Props = BaseProps & {
+//   socialProviders: SocialProvider[];
+//   children: ReactNode;
+// };
+
+
+// const ViewTermsButton = () => {
+//   const handleViewTerms = () => {
+//     window.open('/agreement', '_blank');
+//   };
+//   const handleUsageRules = () => {
+//     window.open('/usage-rules', '_blank');
+//   };
+//   const { t } = useTranslation();
+
+//   return (
+//     <div className="flex flex-row justify-center space-x-4">
+//       <Button onClick={handleViewTerms} variation="link">
+//         {t('auth.viewTerms')}
+//       </Button>
+//       <Button onClick={handleUsageRules} variation="link">
+//         {t('auth.viewRules')}
+//       </Button>
+//     </div>
+//   );
+// };
+
+
+// const AuthAmplify: React.FC<Props> = ({ socialProviders, children }) => {
+
+//   const { t } = useTranslation();
+//   const { signOut } = useAuthenticator();
+
+//   const components = {
+//     Header() {
+//       return (
+//         <div className="mb-5 mt-10 flex justify-center text-2xl text-aws-font-color">
+//           {!MISTRAL_ENABLED ? t('app.name') : t('app.nameWithoutClaude')}
+//         </div>
+//       );
+//     },
+//     Footer() {
+//       const { tokens } = useTheme();
+//       return (
+//         <View textAlign="center" padding={tokens.space.large}>
+//           <Text color={tokens.colors.neutral[80]}>
+//             &copy; All Rights Reserved
+//           </Text>
+//         </View>
+//       );
+//     },
+//     SignUp: {
+//       FormFields() {
+//         const { tokens } = useTheme();
+//         return (
+//           <>
+//             {/* Re-use default `Authenticator.SignUp.FormFields` */}
+//             <Authenticator.SignUp.FormFields />
+
+//             {/* Append & require Terms and Conditions field to sign in  */}
+//             <ViewTermsButton />
+//             <Text color={tokens.colors.neutral[80]}>
+//               By create account of this research platform you must agree & follow "user agreement" & "usage rules". 
+//             </Text>
+//           </>
+//         );
+//       },
+//     },
+//   };
+
+//   // const formFields = {
+//   //    signUp: {
+//   //      email: {
+//   //        order:1
+//   //      },
+//   //      phone_number: {
+//   //        order:2,
+//   //        dialCode: ['+86']
+//   //      },
+//   //      password: {
+//   //        order: 3
+//   //      },
+//   //      confirm_password: {
+//   //        order: 4
+//   //      }
+//   //    },
+//   // };
+
+//   return (
+//     <Authenticator
+//       socialProviders={socialProviders}
+//       initialState="signIn"
+//       components={components}
+//       // formFields={formFields} 
+//     >
+//       <>{cloneElement(children as ReactElement, { signOut })}</>
+//     </Authenticator>
+//   );
+// };
+
+// export default AuthAmplify;
+
+
+import React, { ReactNode, cloneElement, ReactElement } from 'react';
 import { BaseProps } from '../@types/common';
 import { useTranslation } from 'react-i18next';
 import { SocialProvider } from '../@types/auth';
 import { 
   Authenticator, 
   useAuthenticator, 
-  CheckboxField,
   useTheme,
   View,
   Text,
@@ -16,128 +136,104 @@ import {
 const MISTRAL_ENABLED: boolean =
   import.meta.env.VITE_APP_ENABLE_MISTRAL === 'true';
 
-
 type Props = BaseProps & {
   socialProviders: SocialProvider[];
   children: ReactNode;
 };
 
-
 const ViewTermsButton = () => {
   const handleViewTerms = () => {
     window.open('/agreement', '_blank');
   };
-
+  const handleUsageRules = () => {
+    window.open('/usage-rules', '_blank');
+  };
   const { t } = useTranslation();
 
   return (
-    <Button onClick={handleViewTerms} variation="link">
-      {t('auth.viewTerms')}
-    </Button>
+    <div className="flex flex-row justify-center space-x-4">
+      <Button onClick={handleViewTerms} variation="link">
+        {t('auth.viewTerms')}
+      </Button>
+      <Button onClick={handleUsageRules} variation="link">
+        {t('auth.viewRules')}
+      </Button>
+    </div>
   );
 };
 
-
 const AuthAmplify: React.FC<Props> = ({ socialProviders, children }) => {
-
   const { t } = useTranslation();
   const { signOut } = useAuthenticator();
-
-  const components = {
-    Header() {
-      return (
-        <div className="mb-5 mt-10 flex justify-center text-3xl text-aws-font-color">
-          {!MISTRAL_ENABLED ? t('app.name') : t('app.nameWithoutClaude')}
-        </div>
-      );
-    },
-    Footer() {
-      const { tokens } = useTheme();
-      return (
-        <View textAlign="center" padding={tokens.space.large}>
-          <Text color={tokens.colors.neutral[80]}>
-            &copy; All Rights Reserved
-          </Text>
-        </View>
-      );
-    },
-    SignUp: {
-      FormFields() {
-
-        const { validationErrors } = useAuthenticator();
-        const [isChecked, setIsChecked] = useState(false);
-        const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-          setIsChecked(event.target.checked);
-        };
-
-        return (
-          <>
-            {/* Re-use default `Authenticator.SignUp.FormFields` */}
-            <Authenticator.SignUp.FormFields />
-
-            {/* Append & require Terms and Conditions field to sign in  */}
-            <ViewTermsButton />
-            <div>
-              {/* Append & require Terms and Conditions field to sign in  */}
-              <CheckboxField
-                errorMessage={validationErrors.acknowledgement as string}
-                hasError={!!validationErrors.acknowledgement}
-                name="acknowledgement"
-                value="yes"
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-                label={
-                  <>
-                    {t('auth.agreeTerms')} 
-                  </>
-                }
-              />
-            </div>
-          </>
-        );
-      },
-    },
-  };
-
-  const services = {
-    async validateCustomSignUp(formData: Record<string, any>) {
-      if (!formData.acknowledgement) {
-        return {
-          acknowledgement: t('auth.errors.mustAgreeTerms'),
-        };
-      }
-    },
-  };
-
-  // const formFields = {
-  //    signUp: {
-  //      email: {
-  //        order:1
-  //      },
-  //      phone_number: {
-  //        order:2,
-  //        dialCodeList: ['+86', '+852', '+1']
-  //      },
-  //      password: {
-  //        order: 3
-  //      },
-  //      confirm_password: {
-  //        order: 4
-  //      }
-  //    },
-  // };
 
   return (
     <Authenticator
       socialProviders={socialProviders}
-      initialState="signIn"
-      components={components}
-      services={services}
-      // formFields={formFields} 
-    >
+      components={{
+        Header: () => (
+          <div className="mb-5 mt-10 flex justify-center text-3xl text-aws-font-color">
+            {!MISTRAL_ENABLED ? t('app.name') : t('app.nameWithoutClaude')}
+          </div>
+        ),
+        Footer() {
+          const { tokens } = useTheme();
+          return (
+           <>
+            <Text color={tokens.colors.neutral[80]} padding={tokens.space.large} textAlign="justify">
+              By create account of this research platform you must agree & follow "user agreement" & "usage rules". 
+            </Text>
+            <ViewTermsButton />
+            <Text color={tokens.colors.neutral[80]} padding={tokens.space.large} textAlign="justify">
+              This platform has been developed and deployed for research purposes. Features and functionality may be updated periodically without prior notice. Please note that we do not offer any Service Level Agreement (SLA) for this platform.
+            </Text>
+            <View textAlign="center" padding={tokens.space.large}>
+              <Text color={tokens.colors.neutral[80]}>
+                Amazon Research &copy; All Rights Reserved. 
+              </Text>
+            </View>
+           </>
+          );
+        },
+      }}>
       <>{cloneElement(children as ReactElement, { signOut })}</>
     </Authenticator>
   );
 };
 
 export default AuthAmplify;
+
+// import React, { ReactNode, cloneElement, ReactElement } from 'react';
+// import { BaseProps } from '../@types/common';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import { useTranslation } from 'react-i18next';
+// import { useAuthenticator } from '@aws-amplify/ui-react';
+// import { SocialProvider } from '../@types/auth';
+
+// const MISTRAL_ENABLED: boolean =
+//   import.meta.env.VITE_APP_ENABLE_MISTRAL === 'true';
+
+// type Props = BaseProps & {
+//   socialProviders: SocialProvider[];
+//   children: ReactNode;
+// };
+
+// const AuthAmplify: React.FC<Props> = ({ socialProviders, children }) => {
+//   const { t } = useTranslation();
+//   const { signOut } = useAuthenticator();
+
+//   return (
+//     <Authenticator
+//       socialProviders={socialProviders}
+//       components={{
+//         Header: () => (
+//           <div className="mb-5 mt-10 flex justify-center text-3xl text-aws-font-color">
+//             {!MISTRAL_ENABLED ? t('app.name') : t('app.nameWithoutClaude')}
+//           </div>
+//         ),
+//       }}>
+//       <>{cloneElement(children as ReactElement, { signOut })}</>
+//     </Authenticator>
+//   );
+// };
+
+// export default AuthAmplify;
